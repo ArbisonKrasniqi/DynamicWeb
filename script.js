6f6d5c3a1edf5d68db323f9ff9245f6d7e95f883
ArbisonKrasniqi <ak64951@ubt-uni.net>
2023-11-19 18:20:45 +0100

    last github test

--- script.js
+++ script.js
@@ -1,35 +1,16 @@
-function getData(filename, div) {
-    return fetch(filename)
+function loadExternalHTML(url, targetElementId) {
+    fetch(url)
         .then(response => response.text())
         .then(html => {
-            // Set the innerHTML of the specified div element
-            document.getElementById(div).innerHTML = html;
-            // Return the innerHTML
-            return html;
+            document.getElementById(targetElementId).innerHTML = html;
         })
-        .catch(error => {
-            console.error("Error loading HTML:", error);
-            throw error; // Re-throw the error to propagate it to the next catch block
-        });
+        .catch(error => console.error('Error loading HTML: ', error));
 }
 
-getData('index2.html', 'test')
-    .then(html => {
-        // Do something with the fetched HTML
-        console.log(html);
-    })
-    .catch(error => {
-        // Handle errors
-        console.error(error);
-    });
-
-// Example usage
 function changeContent(tab) {
     var contentDiv = document.getElementById('dynamic');
     console.log("Clicked tab "+tab);
 
-    
-    // Example usage
     switch (tab) {
         case 'menu':
             contentDiv.innerHTML = "<p> menu </p>"; //Menu here
@@ -40,7 +21,12 @@ function changeContent(tab) {
             break;
 
         case 'genres':
-            contentDiv.innerHTML = getData("index2.html", "test");
+            fetch('index2.html')
+                .then(response => response.text())
+                .then(html => {
+                    contentDiv.innerHTML = html;
+                })
+                .catch(error => console.error('Error loading HTML:', error));
             break;
 
         case 'settings':
