bf3164efe33cdde6f1bb6c166f7e74e6fc6825fc
ArbisonKrasniqi <ak64951@ubt-uni.net>
2023-11-19 15:54:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function loadExternalHTML(url, targetElementId) {
+    fetch(url)
+        .then(response => response.text())
+        .then(html => {
+            // Inject the HTML into the specified target element
+            document.getElementById(targetElementId).innerHTML = html;
+        })
+        .catch(error => console.error('Error loading HTML:', error));
+}
+
+// Example usage
+
+function changeContent(tab) {
+    var contentDiv = document.getElementById('dynamic');
+    console.log("Clicked tab "+tab);
+
+    
+    // Example usage
+    switch (tab) {
+        case 'menu':
+            contentDiv.innerHTML = "<p> menu </p>"; //Menu here
+            break;
+
+        case 'songs':
+            contentDiv.innerHTML = "songs"; //Songs Here
+            break;
+
+        case 'genres':
+            contentDiv.innerHTML = loadExternalHTML("index2.html", "test");
+            break;
+
+        case 'settings':
+            contentDiv.innerHTML = "settings"; //Settings here
+            break;
+        
+        default:
+            break;
+    }
+}
